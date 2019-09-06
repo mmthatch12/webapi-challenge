04d1bd26d2d7c3f5298eaf1a055df893dd853841
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-06 11:26:03 -0600

    added logger global middleware function

--- api/server.js
+++ api/server.js
@@ -6,10 +6,17 @@ console.log('environment', process.env.NODE_ENV)
 const actionRouter = require('../data/helpers/actionRouter')
 const projectRouter = require('../data/helpers/projectRouter')
 
+function logger(req, res, next) {
+    console.log(`${req.method} to ${req.url} at [${new Date().toISOString()}] `)
+  
+    next()
+  }
+
 const server = express()
 
 server.use(express.json())
 server.use(cors())
+server.use(logger)
 
 server.use('/action', actionRouter)
 server.use('/project', projectRouter)
