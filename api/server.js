71e1d35425262406329ae69599fda937d2f25960
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-06 11:20:53 -0600

    deployed app to heroku

--- api/server.js
+++ api/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const cors = require('cors')
+
+console.log('environment', process.env.NODE_ENV)
 
 const actionRouter = require('../data/helpers/actionRouter')
 const projectRouter = require('../data/helpers/projectRouter')
@@ -6,17 +9,13 @@ const projectRouter = require('../data/helpers/projectRouter')
 const server = express()
 
 server.use(express.json())
+server.use(cors())
 
 server.use('/action', actionRouter)
 server.use('/project', projectRouter)
 
-
-
-
-
 server.get('/', (req, res) => {
     res.send(`<h1>Sanity check<h1>`)
 })
-
 
 module.exports = server
