08d49eaa52080c20473d322db118237f94a5a003
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-06 10:31:34 -0600

    added first get for actionRouter

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -2,7 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
+const actionDB = require('./actionModel')
 
+router.get('/', (req, res) => {
+    actionDB.get()
+        .then(action => {
+            res.status(200).json(action)
+        })
+        .catch(error => {
+            res.status(500).json({ error: "Could not load projects"})
+        })
+})
 
 
 
