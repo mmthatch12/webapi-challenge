b3681b7655669d35d4d5278c3759422ec9bb2c68
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-06 09:48:54 -0600

    added first .get to projectRouter

--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
+const projectDB = require('./projectModel')
+
+router.get('/', (req, res) => {
+    projectDB.get()
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(error => {
+            res.status(500).json({ error: "Could not load projects"})
+        })
+})
+
 
 
 
